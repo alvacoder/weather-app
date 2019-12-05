e1903b981dfb90b9c3fc81ac035ff3742d4c6e36
realprinceviper <idrisadeniji01@gmail.com>
2019-12-05 09:19:00 -0500

    accept address from user input

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+            .options({
+                a: {
+                    demand:true,
+                    alias: 'address',
+                    describe: 'Address to fetch weather for',
+                    string: true
+                }
+            })
+            .help()
+            .alias('help', 'h')
+            .argv;
+
+console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=140%20ijesha%20road',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    //console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
+
